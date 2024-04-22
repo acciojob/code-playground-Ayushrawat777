cefccabd368dc163af169e7c6897c171b1a9d722
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-22 17:44:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,76 @@
-
 import React from "react";
-import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+// react router dom version 5 was used here
+
+import Login from "./Login";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
+// Custom PrivateRoute component
+const PrivateRoute = ({ isAuthenticated }) => {
+  return isAuthenticated ? <Outlet /> : <Navigate to={"/login"} />;
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  }
+
+  render() {
+    const { isLoggedIn } = this.state;
+    return (
+      <Router>
+        <div className="main-container">
+          <div>
+            {isLoggedIn
+              ? "Logged in, Now you can enter Playground"
+              : "You are not authenticated, Please login first"}
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">PlayGround</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route
+              path="/login"
+              element={<Login login={this.login} isLogged={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/"
+              element={<PrivateRoute isAuthenticated={isLoggedIn} />}
+            >
+              <Route exact path="/" element={<Home />} />
+            </Route>
+
+            <Route element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App
+export default App;
