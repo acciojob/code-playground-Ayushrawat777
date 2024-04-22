4cf8222a36ca672357d9b5b668ffc7eafbe2a404
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-22 22:16:18 +0530

    Update and rename login.js to Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login = ({isLoggedIn ,SetisLoggedIn}) => {
+  return (
+    <div>
+      <p>Login</p>
+      <button
+        onClick={() => {
+          SetisLoggedIn(!isLoggedIn);
+        }}
+      >
+        {!isLoggedIn ? "Log In" : "Log Out"}
+      </button>
+    </div>
+  );
+};
+
+export default Login;
+
