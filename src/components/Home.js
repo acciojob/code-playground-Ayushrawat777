cc53a4940c4b71a17f0d321daee1826b0c1ae4c5
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 14:43:15 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-    Hi Welcome to Code Playground
+      <h2>Welcome to the Home Page</h2>
+      <p>This is a protected route. Only logged-in users can see this.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
