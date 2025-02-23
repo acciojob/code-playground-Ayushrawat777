e84bbebf366d0071dc2c001dd922a0d97356b866
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 14:43:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,26 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
- NavLink,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
-// react router dom version 5 was used here
-import Login from "./Login";
+import "../styles/App.css";
 import Home from "./Home";
-import NotFound from "./NotFound";
-
-const PrivateRoute = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to={"/login"} />;
-};
+import Login from "./Login";
+import Navigation from "./Navigation";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
-  const [isLoggedIn, SetisLoggedIn] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <div className="main-container">
-        <div>
-          {isLoggedIn
-            ? "Logged in, Now you can enter Playground"
-            : "You are not authenticated, Please login first"}
-        </div>
-        <ul>
-          <li>
-            <NavLink to="/">PlayGround</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </ul>
-
-        <Routes>
-          <Route
-            exact
-            path="/login"
-            element={
-              <Login isLoggedIn={isLoggedIn} SetisLoggedIn={SetisLoggedIn} />
-            }
-          />
-
-          <Route
-            exact
-            path="/"
-            element={<PrivateRoute isAuthenticated={isLoggedIn} />}
-          >
-            <Route exact path="/" element={<Home />} />
+        <Navigation isAuthenticated={isAuthenticated} />
+        <Switch>
+          <Route path="/login">
+            <Login setIsAuthenticated={setIsAuthenticated} />
           </Route>
-          <Route
-           path="*" element={<NotFound />} />
-        </Routes>
+          <PrivateRoute path="/home" component={Home} isAuthenticated={isAuthenticated} />
+          <Redirect from="/" to="/login" />
+        </Switch>
       </div>
     </Router>
   );
